5b8a5447cd533e7be595f70ad30a0d9da6a66a6a
Hai Tran <hai@bio-rithm.com>
2021-01-13 16:27:07 +0700

    refactor HeartRateTableViewTable and SimulatedHeartRateModel

--- SwiftUIAmplify/ecg/CTGPaperView.swift
+++ SwiftUIAmplify/ecg/CTGPaperView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by hai on 12/1/21.
 //  Copyright © 2021 biorithm. All rights reserved.
-//
+//  13 JAN 2021 created SimulatedHeartRateModel and HeartRateTableViewModel 
 
 import SwiftUI
 import Amplify
@@ -14,6 +14,98 @@ import AmplifyPlugins
 // TODO constant.swift
 let timerPeriod: Int = 1
 let maxScreenLength : Int = 4800
+
+class SimulateHeartRateModel : ObservableObject {
+    private var constBeats = [Double]()
+    @Published var beats = [Double]()
+    let numBeatPerUpdate = 20
+    var counter: Int = 0
+    
+    func simulateRealTimeHeartRate(){
+        if self.constBeats.count == 0 {
+            self.loadHeartRateFromFile()
+            self.constBeats = self.beats
+            self.beats.removeAll()
+        }
+        
+        if self.constBeats.count > (self.counter + 1) * self.numBeatPerUpdate {
+            for i in 0..<self.numBeatPerUpdate {
+                self.beats.append(self.constBeats[Int(self.counter * self.numBeatPerUpdate) + i])
+            }
+            self.counter += 1
+        } else {
+//            self.isActive = false
+        }
+    }
+    
+    func loadHeartRateFromFile() {
+        var allWords = [String]()
+        if let startWordsURL = Bundle.main.url(forResource: "heartrate", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+                for i in 0..<allWords.count {
+                    let beat = Double(allWords[i]) ?? 0.0
+                    self.beats.append(beat==Double.nan ? 0.0 : beat)
+                }
+            }
+        }
+        if allWords.isEmpty {
+            allWords = ["NaN"]
+            print(allWords)
+        }
+    }
+    
+}
+
+class HeartRateViewModel : ObservableObject {
+    var isoformatter = ISO8601DateFormatter.init()
+    private var bufferBeats = [HeartRate]()
+    @Published var bufferMHR = [Double]()
+    @Published var bufferTime = [Temporal.DateTime]()
+    @Published var bufferFHR = [Double]()
+    
+    func loadHeartRateFromDB() {
+        Amplify.DataStore.query(HeartRate.self){result in
+            switch result {
+            case .success(let beats):
+                self.bufferBeats = beats.sorted(by: { $0.time![0].foundationDate > $1.time![0].foundationDate })
+                for beat in self.bufferBeats {
+                    self.bufferFHR.append(contentsOf: beat.fHR ?? [0.0])
+                    self.bufferMHR.append(contentsOf: beat.mHR ?? [0.0])
+                    self.bufferTime.append(contentsOf: beat.time!)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    @State var token: AnyCancellable?
+        func observeHeartRatesDBTable(){
+            token = Amplify.DataStore.publisher(for: HeartRate.self).sink(
+                receiveCompletion: {
+                print($0)
+            }, receiveValue: { event in
+                do {
+                    let beats = try event.decodeModel(as: HeartRate.self)
+                    self.bufferBeats.append(beats)
+                    self.bufferBeats = self.bufferBeats.sorted(by: { $0.time![0].foundationDate > $1.time![0].foundationDate })
+                    
+                    self.bufferFHR.removeAll()
+                    self.bufferMHR.removeAll()
+                    self.bufferTime.removeAll()
+                    
+                    for beat in self.bufferBeats {
+                        self.bufferFHR.append(contentsOf: beat.fHR ?? [0.0])
+                        self.bufferMHR.append(contentsOf: beat.mHR ?? [0.0])
+                        self.bufferTime.append(contentsOf: beat.time!)
+                    }
+                } catch {
+                    print(error)
+                }
+            })
+        }
+}
 
 struct HeartRateLine : Shape {
     @State var beats: [Double]
@@ -94,20 +186,9 @@ struct TimeAxis : View {
 }
 
 struct CTGPaperView: View {
-    //
-    var isoformatter = ISO8601DateFormatter.init()
-    @State var bufferBeats = [HeartRate]()
-    @State var bufferTime = [Temporal.DateTime]()
-    @State var bufferMHR: [Double] = [Double]()
-    @State var bufferFHR: [Double] = [Double]()
+    @ObservedObject var heartRateTableModel = HeartRateViewModel()
+    @ObservedObject var simulateHeartRateModel = SimulateHeartRateModel()
     
-    let numBeatPerUpdate: Int = 20
-    @State private var isActive = false
-    @State var constBeats: [Double] = [150]
-    let timer = Timer.publish(every: TimeInterval(timerPeriod),
-                              on: .main, in: .common).autoconnect()
-    @State var counter: Int = 0
-    @State var beats: [Double] = [150]
     var body: some View {
         ZStack(){
             Color.white.opacity(0.2).edgesIgnoringSafeArea(.all)
@@ -124,108 +205,17 @@ struct CTGPaperView: View {
                         .padding(.leading, 5)
                 }
                 
-                HeartRateLine(beats: self.bufferMHR)
+                HeartRateLine(beats: self.simulateHeartRateModel.beats)
                     .stroke(Color.red, lineWidth: 1.2)
             }
             .onAppear(perform: {
-                self.loadHeartRates()
-                self.observeHeartRates()
-//                self.constBeats = self.loadHeartRateFromFile()
-//                self.beats = self.constBeats
-            })
-                .onReceive(timer, perform: {_ in
-                    guard self.isActive else {return}
-                    self.counter = self.counter + 1
-                    self.simulateRealTimeHeartRate()
+                _ = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
+                    self.simulateHeartRateModel.simulateRealTimeHeartRate()
                 })
-                .onReceive(NotificationCenter.default.publisher(
-                    for: UIApplication.willResignActiveNotification)){_ in
-                        self.isActive = false
-            }
-            .onReceive(NotificationCenter.default.publisher(
-                for: UIApplication.willEnterForegroundNotification)){_ in
-                    self.isActive = true
+//                self.simulateHeartRateModel.loadHeartRateFromFile()
+//                self.heartRateTableModel.loadHeartRateFromFile()
+            })
             }
         }
-    }
-    
-    func loadHeartRate() {
-        for i in 0...100 {
-            self.beats.append(Double(150 + i))
-        }
-    }
-    
-    func loadHeartRateFromFile() -> [Double] {
-        var allWords = [String]()
-        var beats = [Double]()
-        
-        if let startWordsURL = Bundle.main.url(forResource: "heartrate", withExtension: "txt") {
-            if let startWords = try? String(contentsOf: startWordsURL) {
-                allWords = startWords.components(separatedBy: "\n")
-                for i in 0..<allWords.count {
-                    let beat = Double(allWords[i]) ?? 0.0
-                    beats.append(beat==Double.nan ? 0.0 : beat)
-                }
-            }
-        }
-        if allWords.isEmpty {
-            allWords = ["NaN"]
-            print(allWords)
-        }
-        return beats
-    }
-    
-    func simulateRealTimeHeartRate(){
-        print(self.beats.count)
-        if self.constBeats.count > (self.counter + 1) * self.numBeatPerUpdate {
-            for i in 0..<self.numBeatPerUpdate {
-                self.beats.append(self.constBeats[Int(self.counter * self.numBeatPerUpdate) + i])
-            }
-        } else {
-            self.isActive = false
-        }
-    }
-    
-     func loadHeartRates() {
-           Amplify.DataStore.query(HeartRate.self){result in
-               switch result {
-               case .success(let beats):
-                   self.bufferBeats = beats.sorted(by: { $0.time![0].foundationDate > $1.time![0].foundationDate })
-                   for beat in self.bufferBeats {
-                       self.bufferFHR.append(contentsOf: beat.fHR ?? [0.0])
-                       self.bufferMHR.append(contentsOf: beat.mHR ?? [0.0])
-                       self.bufferTime.append(contentsOf: beat.time!)
-                   }
-               case .failure(let error):
-                   print(error)
-               }
-           }
-       }
-    
-    @State var token: AnyCancellable?
-     func observeHeartRates(){
-         token = Amplify.DataStore.publisher(for: HeartRate.self).sink(
-             receiveCompletion: {
-             print($0)
-         }, receiveValue: { event in
-             do {
-                 let beats = try event.decodeModel(as: HeartRate.self)
-                 self.bufferBeats.append(beats)
-                 self.bufferBeats = self.bufferBeats.sorted(by: { $0.time![0].foundationDate > $1.time![0].foundationDate })
-                 
-                 self.bufferFHR.removeAll()
-                 self.bufferMHR.removeAll()
-                 self.bufferTime.removeAll()
-                 
-                 for beat in self.bufferBeats {
-                     self.bufferFHR.append(contentsOf: beat.fHR ?? [0.0])
-                     self.bufferMHR.append(contentsOf: beat.mHR ?? [0.0])
-                     self.bufferTime.append(contentsOf: beat.time!)
-                 }
-             } catch {
-                 print(error)
-             }
-         })
-     }
 }
 
